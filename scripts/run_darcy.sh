c0d91467544e4c534755e64eb2ddbdc15aa0a9c0
crwhite14 <colin.white2@gmail.com>
2023-05-24 19:52:25 +0000

    update darcy scripts

--- scripts/run_darcy.sh
+++ scripts/run_darcy.sh
@@ -8,4 +8,17 @@ python train_darcy.py --wandb.name=Darcy-5k-regular-main
 python train_darcy.py --wandb.name=Darcy-5k-amp+half-tanh-main --tfno2d.stabilizer='tanh' --tfno2d.half_prec_fourier=True --opt.amp_autocast=True
 
 
+#regular FNO 
+python train_darcy.py --wandb.name=Darcy-5k-regular-main  --distributed.seed=667
+
+#both amp and half precision fourier with tanh stabilizer 
+python train_darcy.py --wandb.name=Darcy-5k-amp+half-tanh-main --tfno2d.stabilizer='tanh' --tfno2d.half_prec_fourier=True --opt.amp_autocast=True --distributed.seed=667
+
+
+#regular FNO 
+python train_darcy.py --wandb.name=Darcy-5k-regular-main --distributed.seed=668
+
+#both amp and half precision fourier with tanh stabilizer 
+python train_darcy.py --wandb.name=Darcy-5k-amp+half-tanh-main --tfno2d.stabilizer='tanh' --tfno2d.half_prec_fourier=True --opt.amp_autocast=True --distributed.seed=668
+
 
