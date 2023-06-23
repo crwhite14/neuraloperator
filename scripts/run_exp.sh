3e0f81a1f2d0d550bd649723766a0b90a036f6cc
renbo <rtu715@hotmail.com>
2023-06-23 11:47:54 -0400

    testing on 1024 resolution

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python train_navier_stokes.py --wandb.name=NS-10k-amp+half-tanh-tensorized --tfno2d.stabilizer='tanh' --tfno2d.half_prec_fourier=True --tfno2d.half_prec_inverse=True --opt.amp_autocast=True
+#tensorized with MP 
+python train_navier_stokes.py --wandb.name=NS-10k-mp-tensorized-1024test --tfno2d.stabilizer='tanh' --tfno2d.half_prec_fourier=True --tfno2d.half_prec_inverse=True --opt.amp_autocast=True
+
+#tensorized with FP 
+python train_navier_stokes.py --wandb.name=NS-10k-full-tensorized-1024test
